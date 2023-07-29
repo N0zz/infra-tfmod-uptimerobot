72fb96a0cff1c427088966cf6310b4a61b5df002
Krzysztof Kolodziejczyk <kkolodziejczyk@gog.com>
2023-07-30 00:36:14 +0200

    fmt+gitignore

--- main.tf
+++ main.tf
@@ -1,59 +1,59 @@
 locals {
-    monitor_opts = [
-        for dns_record in var.dns_records: {
-            url = format("%s%s%s", dns_record.subdomain, (dns_record.subdomain != "" ? "." : ""), dns_record.zone)
-        }
-    ]
+  monitor_opts = [
+    for dns_record in var.dns_records : {
+      url = format("%s%s%s", dns_record.subdomain, (dns_record.subdomain != "" ? "." : ""), dns_record.zone)
+    }
+  ]
 }
 
 resource "uptimerobot_alert_contact" "email" {
-    count         = var.uptimerobot_alert_email != "disabled" ? 1 : 0
-    friendly_name = var.uptimerobot_alert_email
-    type          = "e-mail"
-    value         = var.uptimerobot_alert_email
+  count         = var.uptimerobot_alert_email != "disabled" ? 1 : 0
+  friendly_name = var.uptimerobot_alert_email
+  type          = "e-mail"
+  value         = var.uptimerobot_alert_email
 }
 resource "uptimerobot_monitor" "https_monitors" {
-    for_each = {for k, v in var.dns_records :  k => v if v.subdomain != "*"}
+  for_each = { for k, v in var.dns_records : k => v if v.subdomain != "*" }
 
-    url           = format("https://%s", local.monitor_opts[index(keys(var.dns_records), each.key)].url)
-    friendly_name = format("HTTPS %s TF", local.monitor_opts[index(keys(var.dns_records), each.key)].url)
-    type          = "http"
-    interval      = "300"
+  url           = format("https://%s", local.monitor_opts[index(keys(var.dns_records), each.key)].url)
+  friendly_name = format("HTTPS %s TF", local.monitor_opts[index(keys(var.dns_records), each.key)].url)
+  type          = "http"
+  interval      = "300"
 
-    dynamic "alert_contact" {
-        for_each = var.uptimerobot_alert_email != "disabled" ? toset([1]) : toset([])
-        content {
-            id = uptimerobot_alert_contact.email[0].id
-            threshold = 0
-            recurrence = 0
-        }
+  dynamic "alert_contact" {
+    for_each = var.uptimerobot_alert_email != "disabled" ? toset([1]) : toset([])
+    content {
+      id         = uptimerobot_alert_contact.email[0].id
+      threshold  = 0
+      recurrence = 0
     }
+  }
 
 }
 
 resource "uptimerobot_monitor" "custom_monitors" {
-    for_each = var.uptimerobot_custom_monitors
+  for_each = var.uptimerobot_custom_monitors
 
-    url           = each.value.url
-    friendly_name = format("%s TF", each.value.name)
-    type          = each.value.type
-    sub_type      = each.value.sub_type
-    interval      = try(each.value.interval, null)
-    port          = each.value.sub_type == "custom" ? try(each.value.port, null) : null
+  url           = each.value.url
+  friendly_name = format("%s TF", each.value.name)
+  type          = each.value.type
+  sub_type      = each.value.sub_type
+  interval      = try(each.value.interval, null)
+  port          = each.value.sub_type == "custom" ? try(each.value.port, null) : null
 
-    dynamic "alert_contact" {
-        for_each = var.uptimerobot_alert_email != "disabled" ? toset([1]) : toset([])
-        content {
-            id = uptimerobot_alert_contact.email[0].id
-            threshold = 0
-            recurrence = 0
-        }
+  dynamic "alert_contact" {
+    for_each = var.uptimerobot_alert_email != "disabled" ? toset([1]) : toset([])
+    content {
+      id         = uptimerobot_alert_contact.email[0].id
+      threshold  = 0
+      recurrence = 0
     }
+  }
 
 }
 resource "uptimerobot_status_page" "status_page" {
-    count = var.environment == "prod" ? 1 : 0
-    friendly_name  = "Main Status Page"
-    sort           = "down-up-paused"
-    monitors       = [0]
+  count         = var.environment == "prod" ? 1 : 0
+  friendly_name = "Main Status Page"
+  sort          = "down-up-paused"
+  monitors      = [0]
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "environment" {
-    description = "Used to condition resources which should only be created in specific env."
-    type = string
+  description = "Used to condition resources which should only be created in specific env."
+  type        = string
 }
 variable "dns_records" {
-    description = "Creates default monitors and alerts for DNS records created in OVH."
-    type = map
+  description = "Creates default monitors and alerts for DNS records created in OVH."
+  type        = map(any)
 }
 
 variable "uptimerobot_custom_monitors" {
-    description = "User defined custom monitors."
-    type = map
+  description = "User defined custom monitors."
+  type        = map(any)
 }
 variable "uptimerobot_alert_email" {
-    description = "Email alert to be attached to created monitors."
-    type = string
+  description = "Email alert to be attached to created monitors."
+  type        = string
 }
