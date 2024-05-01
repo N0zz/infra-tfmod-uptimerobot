43252684c37b228b1306377b0c33da36db23fa8b
krzysztof.kolodziejczyk <krzysztof.kolodziejczyk@synerise.com>
2024-05-01 12:22:48 +0200

    feat: rework module

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,21 @@
-output "alert_contact" {
-  value = uptimerobot_alert_contact.email
-}
-output "https_monitors" {
+output "default_monitors" {
   value = {
-    for name, status in uptimerobot_monitor.https_monitors : name => status
+    for name, status in uptimerobot_monitor.default_monitors : name => status
   }
 }
+
 output "custom_monitors" {
   value = {
     for name, status in uptimerobot_monitor.custom_monitors : name => status
   }
 }
+
 output "status_page" {
-  value = uptimerobot_status_page.status_page
+  value = var.enable_status_page ? uptimerobot_status_page.status_page : []
+}
+
+output "alert_contacts" {
+  value = {
+    for id, value in data.uptimerobot_alert_contact.alert_contacts : id => value.id
+  }
 }
